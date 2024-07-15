9ab866227a3e2653480d5fb88d5bf9c8971a0750
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-07-15 17:35:56 +0300

    Add file via upload

---
+++ 01. JS Syntax Fundamentals – Lab/05.math-operations.js
@@ -0,0 +1,31 @@
+function task(num1, num2, operator) {
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        case '%':
+            result = num1 % num2;
+            break;
+        case '**':
+            result = num1 ** num2;
+            break;
+        default:
+            result = 'Invalid operator!';
+            break;
+    }
+
+    console.log(result);
+}
+
+task(5, 5, '+')
