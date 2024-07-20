d931fa9247c54bc6321e1ffb5d8dc5410b7eac1b
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-07-20 09:47:16 +0300

    Add files via upload

---
+++ 02 .JS Syntax Fundamentals - Exercise/01.ages.js
@@ -0,0 +1,21 @@
+function check(age) {
+    let result;
+
+    if (age >= 0 && age <= 2) {
+        result = 'baby';
+    } else if (age > 2 && age <= 13) {
+        result = 'child';
+    } else if (age > 13 && age <= 19) {
+        result = 'teenager';
+    } else if (age > 19 && age <= 65) {
+        result = 'adult';
+    } else if (age > 65) {
+        result = 'elder';
+    } else {
+        result = 'out of bounds';
+    }
+
+    console.log(result);
+}
+
+check(20);
