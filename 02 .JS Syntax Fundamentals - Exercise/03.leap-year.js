c22554b88e37c50ca70bf6a33d7d14aa8e63018a
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-07-22 00:07:47 +0300

    Add files via upload

---
+++ 02 .JS Syntax Fundamentals - Exercise/03.leap-year.js
@@ -0,0 +1,10 @@
+function check(year) {
+
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log('yes');    
+    } else {
+        console.log('no');
+    }
+}
+
+check(2024);
