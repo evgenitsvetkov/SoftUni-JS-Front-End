10189a52a315c997a0d763d2eb5b68774d6ffcd9
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-07-28 23:45:32 +0300

    Add files via upload

---
+++ 03. JS Arrays and Strings – Lab/01.sum-first-and-last-array-elements.js
@@ -0,0 +1,13 @@
+function sumArray(arr) {
+    
+
+    let first = arr[0];
+    let last = arr[arr.length - 1];
+
+    let result = first + last;
+
+    console.log(result);
+
+}
+
+sumArray([20, 30, 40])
