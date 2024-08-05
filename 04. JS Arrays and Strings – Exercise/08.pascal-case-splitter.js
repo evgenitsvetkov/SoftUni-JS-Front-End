3a95368880f95dd04560203f378423581443d23f
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-08-05 16:09:30 +0300

    Add files via upload

---
+++ 04. JS Arrays and Strings – Exercise/08.pascal-case-splitter.js
@@ -0,0 +1,22 @@
+function splitter(input) {
+    input = input.split('');
+
+    let words = [];
+
+    let counter = -1;       
+
+    input.map(x => {
+
+        if (x.charCodeAt(0) >= 65 && x.charCodeAt(0) <= 90) {
+            counter++
+            words[counter] = x;
+        } else if (x.charCodeAt(0) >= 97 && x.charCodeAt(0) <= 122) {
+            words[counter] += x
+        }
+    })
+
+    console.log(words.join(", "));
+}
+
+
+splitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
