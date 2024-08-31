985ce8c89e0b51c33d8071753b5d19e4889364a6
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-08-31 20:12:50 +0300

    Add files via upload

---
+++ 07. Objects and Classes - Lab/01.person-info.js
@@ -0,0 +1,11 @@
+function returnObject(firstName, lastName, age) {
+    const person = {
+        firstName,
+        lastName,
+        age
+    };
+
+    return person;
+}
+
+returnObject('Peter', 'Pan', '20');
