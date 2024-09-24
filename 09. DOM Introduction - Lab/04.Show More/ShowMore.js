68553ef8681827d2501c0f18eb50fe96b46ead57
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-09-24 11:32:45 +0300

    Add files via upload

---
+++ 09. DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const onClickMore = document.getElementById('more');
+    const text = document.getElementById('text');
+
+    onClickMore.style.display = 'none';
+    text.style.display = 'inline';
+}
