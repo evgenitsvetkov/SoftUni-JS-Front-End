c2998523149dc38866bfa55335a3877221c95e47
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-10-05 11:35:58 +0300

    Add files via upload

---
+++ 10. DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -0,0 +1,19 @@
+function solve() {
+    const inputElement = document.getElementById('input');
+    const outputElement = document.getElementById('output');
+
+   function createParagraphElement(text) {
+    const pElement = document.createElement('p');
+    pElement.textContent = text;
+    return pElement;
+  }
+
+   const sentences = inputElement.value.split('.').filter(Boolean).map((sentence) => sentence.trim());
+  
+    for (let i = 0; i < sentences.length; i += 3) {
+      const currentText = sentences.slice(i, i + 3).join('. ').concat('.');
+      const currentParagraph = createParagraphElement(currentText);
+
+      outputElement.appendChild(currentParagraph);
+    }
+}
