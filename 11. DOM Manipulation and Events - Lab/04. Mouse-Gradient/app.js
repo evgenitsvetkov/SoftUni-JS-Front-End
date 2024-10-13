7a0b23c46327548efdcf000e20797581a09a84d4
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-10-13 21:42:04 +0300

    Add files via upload

---
+++ 11. DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
@@ -0,0 +1,12 @@
+function attachGradientEvents() {
+    const resultElement = document.getElementById('result');
+    const gradientElement = document.getElementById('gradient');
+
+    gradientElement.addEventListener('mousemove', (e) => {
+        const currentPosition =  e.offsetX;
+        const elementWidth = e.currentTarget.clientWidth;
+
+        const percent = Math.floor((currentPosition / elementWidth) * 100);
+        resultElement.textContent = `${percent}%`;
+    });
+}
